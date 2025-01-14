b27208d1ef6914f04a4b409be8e0f629fbc8063f
Vitali <vitalrzheutsky@gmail.com>
2025-01-13 21:41:40 -0500

    Implemented portfolio component

--- src/page/Portfolio/Portfolio.jsx
+++ src/page/Portfolio/Portfolio.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow
+} from "@/components/ui/table"
+import { Avatar, AvatarImage } from '@radix-ui/react-avatar';
 
 export const Portfolio = () => {
     return (
-        <div>Portfolio</div>
+        <div className="px-5 lg:px-20">
+            <h1 className="font-bold text-3xl pb-5">Portfolio</h1>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">Asset</TableHead>
+                        <TableHead>Price</TableHead>
+                        <TableHead>Unit</TableHead>
+                        <TableHead>Change</TableHead>
+                        <TableHead>Change%</TableHead>
+                        <TableHead className="text-right">Value</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item, index) =>
+                        <TableRow key={index}>
+                            <TableCell className="font-medium flex items-center gap-3">
+                                <Avatar className='-z-50'>
+                                    <AvatarImage src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/800px-Bitcoin.svg.png' />
+                                </Avatar>
+                                <span>Bitcoin</span>
+                            </TableCell>
+                            <TableCell>BTC</TableCell>
+                            <TableCell>8707416005</TableCell>
+                            <TableCell>1234566789</TableCell>
+                            <TableCell>-0.20009</TableCell>
+
+
+                            <TableCell className="text-right">$80000</TableCell>
+                        </TableRow>
+                    )}
+
+                </TableBody>
+            </Table>
+
+        </div>
     )
 }
