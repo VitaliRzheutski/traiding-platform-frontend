f14ff97258f7ae73b61cd5402630557375444ee8
Vitali <vitalrzheutsky@gmail.com>
2025-01-14 14:29:45 -0500

    Implemented request withdrawal form

--- src/page/Wallet/WithdrawalForm.jsx
+++ src/page/Wallet/WithdrawalForm.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import { Button } from '@/components/ui/button';
+import { DialogClose } from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input'
+import React, { useState } from 'react'
 
 export const WithdrawalForm = () => {
+    const [amount, setAmount] = useState('');
+
+    const handleChange = (e) => {
+        setAmount(e.target.value)
+    }
+    const handleSubmit = () => {
+        console.log(amount)
+    }
     return (
-        <div>WithdrawalForm</div>
+        <div className='pt-10 space-y-5'>
+            <div className='flex justify-between items-center rounded-md bg-slate-900
+            text-xl font-bold px-5 py-4'>
+                <p>Available balance</p>
+                <p>$9000</p>
+            </div>
+            <div className='flex flex-col items-center'>
+                <h1>Enter Withdrawal amount</h1>
+                <div className='flex items-center justify-center'>
+                    <Input
+                        onChange={handleChange}
+                        value={amount}
+                        className="withdrawalInput py-7 border-none focus: outline-none px-0 
+                    text-2xl tex-center"
+                        placeholder="9999"
+                        type="number"
+                    />
+
+                </div>
+            </div>
+            <div>
+                <p className='pb-2'>Transfer to</p>
+                <div className='flex items-center gap-5 border px-5 py-2 rounded-md'>
+                    <img className='h-8 w-8 ' src="https://www.svgrepo.com/show/488788/bank.svg" alt="" />
+                    <div>
+                        <p className='text-xl font-bold'>Bank of America</p>
+                        <p className='text-xs'>***********1124</p>
+                    </div>
+                </div>
+            </div>
+            <DialogClose className='w-full'>
+                <Button onClick={handleSubmit} className="w-full py-7 text-xl">
+                    Withdrawal
+                </Button>
+            </DialogClose>
+
+
+        </div>
     )
 }
