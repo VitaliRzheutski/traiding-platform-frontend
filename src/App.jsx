a9e348bf4c36858017bb73e8ae964856216a4a20
Vitali <vitalrzheutsky@gmail.com>
2025-01-13 21:21:26 -0500

    Fixed bugs with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Navbar from './page/Navbar/Navbar'
-import Home from './page/Home/Home'
-import { Route } from 'react-router-dom'
-import { Portfolio } from './page/Portfolio/Portfolio'
 import { Activity } from './page/Activity/Activity'
-import { Wallet } from './page/Wallet/Wallet'
-import { Withdrawal } from './page/Withdrawal/withdrawal'
-import { PaymentDetals } from './page/Paymentdetails/PaymentDetals'
-import { StockDetails } from './page/StockDetails/StockDetails'
-import { WatchList } from './page/Watchlist/WatchList'
-import { Profile } from './page/Profile/Profile'
-import { SearchCoin } from './page/Search/SearchCoin'
+import Home from './page/Home/Home'
+import Navbar from './page/Navbar/Navbar'
 import { NotFound } from './page/NotFound/NotFound'
+import { PaymentDetals } from './page/Paymentdetails/PaymentDetals'
+import { Portfolio } from './page/Portfolio/Portfolio'
+import { Profile } from './page/Profile/Profile'
+import { StockDetails } from './page/StockDetails/StockDetails'
+import { Wallet } from './page/Wallet/Wallet'
+import { WatchList } from './page/Watchlist/WatchList'
+import { Withdrawal } from './page/Withdrawal/withdrawal'
 
 function App() {
 
@@ -19,9 +18,8 @@ function App() {
     <>
       <Navbar />
       <Routes>
-
         <Route path="/" element={<Home />} />
-        <Route path="/potfolio" element={<Portfolio />} />
+        <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/activity" element={<Activity />} />
         <Route path="/wallet" element={<Wallet />} />
         <Route path="/withdrawal" element={<Withdrawal />} />
@@ -30,14 +28,8 @@ function App() {
         <Route path="/market/:id" element={<StockDetails />} />
         <Route path="/watchList" element={<WatchList />} />
         <Route path="/profile" element={<Profile />} />
-
         <Route path="*" element={<NotFound />} />
-
-
-
-
       </Routes>
-      <Home />
     </>
   )
 }
--- src/page/navbar/SideBar.jsx
+++ src/page/navbar/SideBar.jsx
@@ -3,6 +3,7 @@ import { SheetClose } from '@/components/ui/sheet'
 import { ActivityLogIcon, DashboardIcon, ExitIcon, PersonIcon } from '@radix-ui/react-icons'
 import { BookmarkIcon, HomeIcon, LandmarkIcon, WalletIcon } from 'lucide-react'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const menu = [
     { name: "Home", path: "/", icon: <HomeIcon className='h-6 w-6' /> },
@@ -10,7 +11,7 @@ const menu = [
     { name: "WatchList", path: "/watchlist", icon: <BookmarkIcon className='h-6 w-6' /> },
     { name: "Activity", path: "/activity", icon: <ActivityLogIcon className='h-6 w-6' /> },
     { name: "Wallet", path: "/wallet", icon: <WalletIcon className='h-6 w-6' /> },
-    { name: "Payment Details", path: "/activitypayment-details", icon: <LandmarkIcon className='h-6 w-6' /> },
+    { name: "Payment Details", path: "/payment-details", icon: <LandmarkIcon className='h-6 w-6' /> },
     { name: "Withdrawal", path: "/withdrawal", icon: <LandmarkIcon className='h-6 w-6' /> },
     { name: "Profile", path: "/profile", icon: <PersonIcon className='h-6 w-6' /> },
     { name: "Logout", path: "/", icon: <ExitIcon className='h-6 w-6' /> },
@@ -18,13 +19,20 @@ const menu = [
 ]
 
 const SideBar = () => {
+    const navigate = useNavigate();
+
+
     return (
         <div className='mt-10 space-y-5'>
             <div>
-                {menu.map((el) => {
+                {menu.map((el, index) => {
                     return (
-                        <SheetClose key={el} className='w-full'>
-                            <Button variant="outline" className="flex gap-5 py-6 w-full">
+                        <SheetClose key={index} className='w-full'>
+                            <Button
+                                variant="outline"
+                                className="flex gap-5 py-6 w-full"
+                                onClick={() => navigate(el.path)}
+                            >
                                 <span className='w-8'>
                                     {el.icon}
                                 </span>
