febc2ba53389c9c70fefb1205b9305cfa3f720bd
Vitali <vitalrzheutsky@gmail.com>
2025-01-14 22:27:39 -0500

    Implemented withdrawal component

--- src/page/Withdrawal/Withdrawal.jsx
+++ src/page/Withdrawal/Withdrawal.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
-
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow
+} from "@/components/ui/table"
 export const Withdrawal = () => {
     return (
-        <div>withdrawal</div>
+        <div className="px-5 lg:px-20">
+            <h1 className="font-bold text-3xl pb-5">Withdrawal</h1>
+            <Table className="border">
+                <TableHeader className="py-3" >
+                    <TableRow>
+                        <TableHead className="w-[100px]">Date</TableHead>
+                        <TableHead>Method</TableHead>
+                        <TableHead>Amount</TableHead>
+                        <TableHead className="text-right">Status</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item, index) =>
+                        <TableRow key={index}>
+                            <TableCell><p>January 14, 2024 at 10:24</p> </TableCell>
+                            <TableCell>Bank</TableCell>
+                            <TableCell>-0.20009</TableCell>
+                            <TableCell className="text-right">$80000</TableCell>
+
+                        </TableRow>
+                    )}
+
+                </TableBody>
+            </Table>
+
+        </div>
     )
 }
