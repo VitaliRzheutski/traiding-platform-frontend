8f63905c7f5a8681d83309bee210fecfe93a345a
Vitali <vitalrzheutsky@gmail.com>
2025-01-16 15:46:26 -0500

    Implemented Signin, Singup, Forgot Password components

--- src/App.jsx
+++ src/App.jsx
@@ -11,25 +11,30 @@ import { StockDetails } from './page/StockDetails/StockDetails'
 import { Wallet } from './page/Wallet/Wallet'
 import { WatchList } from './page/Watchlist/WatchList'
 import { Withdrawal } from './page/Withdrawal/Withdrawal'
+import { Auth } from './page/Auth/Auth'
 
 function App() {
 
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/activity" element={<Activity />} />
-        <Route path="/wallet" element={<Wallet />} />
-        <Route path="/withdrawal" element={<Withdrawal />} />
-        <Route path="/payment-details" element={<PaymentDetals />} />
-        <Route path="/withdrawal" element={<Withdrawal />} />
-        <Route path="/market/:id" element={<StockDetails />} />
-        <Route path="/watchList" element={<WatchList />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Auth />
+      {false && <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/activity" element={<Activity />} />
+          <Route path="/wallet" element={<Wallet />} />
+          <Route path="/withdrawal" element={<Withdrawal />} />
+          <Route path="/payment-details" element={<PaymentDetals />} />
+          <Route path="/withdrawal" element={<Withdrawal />} />
+          <Route path="/market/:id" element={<StockDetails />} />
+          <Route path="/watchList" element={<WatchList />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>}
+
     </>
   )
 }
---
+++ src/page/Auth/SigninForm.jsx
@@ -0,0 +1,89 @@
+import { Button } from '@/components/ui/button'
+import { DialogClose } from '@/components/ui/dialog'
+import {
+    Form,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormControl,
+    FormMessage
+} from '@/components/ui/form'
+import {
+    Input
+} from '@/components/ui/input'
+import { useForm } from 'react-hook-form'
+
+export const SigninForm = () => {
+
+    const form = useForm({
+        resolver: "",
+        defaultValues: {
+            fullName: "",
+            email: "",
+            password: "",
+        }
+    })
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+    return (
+        <div >
+            <h1 className='text-xl font-bold text-center pb-3'>Login</h1>
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className='space-y-6'
+                >
+
+
+                    <FormField
+                        control={form.control}
+                        name="Email"
+                        render={({ field }) => (
+                            <FormItem
+                            >
+                                <FormControl>
+                                    <Input
+                                        className="border w-full border-gray-700 p-5"
+                                        placeholder="vitrzh@gamil.com"
+                                        {...field}
+
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+
+
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem
+                            >
+                                <FormControl>
+                                    <Input
+                                        // name="ifsc"
+                                        className="border w-full border-gray-700 p-5"
+                                        placeholder="your password"
+                                        {...field}
+
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+
+                    <Button type="submit" className="w-full py-5">
+                        Submit
+                    </Button>
+
+                </form>
+            </Form>
+        </div>
+    )
+}
