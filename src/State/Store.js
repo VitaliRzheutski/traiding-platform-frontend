dec86871323db528b9106e9fbfa3f2c97106c250
Vitali <vitalrzheutsky@gmail.com>
2025-01-30 16:04:12 -0500

    Implemented redux logic for asset

--- src/State/Store.js
+++ src/State/Store.js
@@ -5,13 +5,15 @@ import coinReducer from "./Coin/Reducer";
 import walletReducer from "./Wallet/Reducer";
 import withdrawalReducer from "./Withdrawal/Reducer";
 import orderReducer from "./Order/Reducer";
+import assetReducer from "./Asset/Reducer";
 
 const rootReducer = combineReducers({
     auth: authReducer,
     coin: coinReducer,
     wallet: walletReducer,
     withdrawal: withdrawalReducer,
-    order: orderReducer
+    order: orderReducer,
+    asset: assetReducer
 });
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
